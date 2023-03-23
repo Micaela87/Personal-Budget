e9c58238e5fd10fc46152edcf0552f6b6444885f
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2023-03-23 11:50:47 +0100

    refactor: separation of category routes from server

--- index.js
+++ index.js
@@ -1,118 +1,17 @@
 import Fastify from 'fastify';
 import { PrismaClient } from './prisma/generated/prisma-client-js/index.js';
-import { bodyPutSchema, bodyPostSchema, responseSchema } from './schemas.js';
+import { getAllCategories, getCategoryByID, postNewCategory, updateCategory, deleteCategory } from './routes/categories.js';
 const prisma = new PrismaClient();
 
 const fastify = Fastify({
   logger: true
 });
 
-fastify.get('/categories', async (req, res) => {
-
-  try {
-
-    const categories = await prisma.budgetCategories.findMany();
-    res.send({ categories });
-
-  } catch(error) {
-
-    fastify.log.error(error);
-    process.exit(1);
-
-  }
-  
-});
-
-fastify.get('/category/:id', async (req, res) => {
-
-  try {
-
-    const category = await prisma.budgetCategories.findUnique({
-      where: { id: Number(req.params.id) }
-    });
-
-    res.send({ category });
-
-  } catch(error) {
-
-    fastify.log.error(error);
-    res.status(500).send({ ok: false, message: error.message });
-    process.exit(1);
-
-  }
-
-});
-
-fastify.post('/category', { handler: async (req, res) => {
-
-  try {
-
-    const bodyValidationFunction = req.getValidationFunction('body');
-    const validationResult = bodyValidationFunction(req.body);
-
-    const category = await prisma.budgetCategories.create({data: req.body});
-
-    const responseSerializationFunction = res.getSerializationFunction(200);
-    const serializedResponse = responseSerializationFunction({category});
-  
-    res.status(200).send(serializedResponse);
-
-  } catch(error) {
-
-    fastify.log.error(error);
-    res.status(500).send({ ok: false, message: error.message });
-    process.exit(1);
-
-  }
-
-}, schema: { body: bodyPostSchema, response: { 200: responseSchema }}});
-
-fastify.put('/category/:id', { handler: async (req, res) => {
-
-  const { id } = req.params;
-
-    try {
-
-      const bodyValidationFunction = req.getValidationFunction('body');
-      const validationResult = bodyValidationFunction(req.body);
-
-      const category = await prisma.budgetCategories.update({
-        where: { id: Number(id) },
-        data: req.body
-      });
-  
-      const responseSerializationFunction = res.getSerializationFunction(200);
-      const serializedResponse = responseSerializationFunction({category});
-  
-      res.status(200).send(serializedResponse);
-  
-    } catch(error) {
-  
-      fastify.log.error(error);
-      res.status(500).send({ ok: false, message: error.message });
-      process.exit(1);
-  
-    }
-
-}, schema: { body: bodyPutSchema, response: { 200: responseSchema }}});
-
-fastify.delete('/category/:id', async (req, res) => {
-
-  try {
-
-    const category = await prisma.budgetCategories.delete({
-      where: { id: Number(req.params.id) }
-    });
-
-    res.send({ category });
-
-  } catch(error) {
-
-    fastify.log.error(error);
-    process.exit(1);
-
-  }
-})
+fastify.register(getAllCategories);
+fastify.register(getCategoryByID);
+fastify.register(postNewCategory);
+fastify.register(updateCategory);
+fastify.register(deleteCategory);
 
 // Run the server!
 fastify.listen({ port: 3000 }, function (err, address) {
---
+++ routes/categories.js
@@ -0,0 +1,140 @@
+import { PrismaClient } from '../prisma/generated/prisma-client-js/index.js';
+import { bodyPutSchema, bodyPostSchema, responseSchema } from '../schemas.js';
+const prisma = new PrismaClient();
+
+export const getAllCategories = (fastify, options = {}, done) => {
+    fastify.get('/categories', async (req, res) => {
+
+        try {
+      
+          const categories = await prisma.budgetCategories.findMany();
+          res.send({ categories });
+      
+        } catch(error) {
+      
+          fastify.log.error(error);
+          process.exit(1);
+      
+        }
+        
+    });
+
+    done();
+}
+
+  
+export const getCategoryByID = (fastify, options = {}, done) => {
+    
+    fastify.get('/category/:id', async (req, res) => {
+  
+        try {
+    
+            const category = await prisma.budgetCategories.findUnique({
+                where: { id: Number(req.params.id) }
+            });
+        
+            res.send({ category });
+    
+        } catch(error) {
+    
+            fastify.log.error(error);
+            res.status(500).send({ ok: false, message: error.message });
+            process.exit(1);
+    
+        }
+    
+    });
+
+    done();
+
+}
+  
+export const postNewCategory = (fastify, options = {}, done) => {
+    
+    fastify.post('/category', { handler: async (req, res) => {
+  
+        try {
+    
+            const bodyValidationFunction = req.getValidationFunction('body');
+            const validationResult = bodyValidationFunction(req.body);
+        
+            const category = await prisma.budgetCategories.create({data: req.body});
+        
+            const responseSerializationFunction = res.getSerializationFunction(200);
+            const serializedResponse = responseSerializationFunction({category});
+            
+            res.status(200).send(serializedResponse);
+    
+        } catch(error) {
+    
+            fastify.log.error(error);
+            res.status(500).send({ ok: false, message: error.message });
+            process.exit(1);
+    
+        }
+  
+    }, schema: { body: bodyPostSchema, response: { 200: responseSchema }}});
+
+    done();
+
+}
+  
+export const updateCategory = (fastify, options = {}, done) => {
+    
+    fastify.put('/category/:id', { handler: async (req, res) => {
+  
+        const { id } = req.params;
+    
+        try {
+    
+            const bodyValidationFunction = req.getValidationFunction('body');
+            const validationResult = bodyValidationFunction(req.body);
+    
+            const category = await prisma.budgetCategories.update({
+            where: { id: Number(id) },
+            data: req.body
+            });
+        
+            const responseSerializationFunction = res.getSerializationFunction(200);
+            const serializedResponse = responseSerializationFunction({category});
+        
+            res.status(200).send(serializedResponse);
+        
+        } catch(error) {
+        
+            fastify.log.error(error);
+            res.status(500).send({ ok: false, message: error.message });
+            process.exit(1);
+        
+        }
+    
+    }, schema: { body: bodyPutSchema, response: { 200: responseSchema }}});
+
+    done();
+
+}
+  
+export const deleteCategory = (fastify, options = {}, done) => {
+    
+    fastify.delete('/category/:id', async (req, res) => {
+  
+        try {
+    
+        const category = await prisma.budgetCategories.delete({
+            where: { id: Number(req.params.id) }
+        });
+    
+        res.status(200).send({ ok: true });
+    
+        } catch(error) {
+    
+        fastify.log.error(error);
+        res.status(500).send({ ok: false, message: error.message });
+        process.exit(1);
+    
+        }
+    });
+
+    done();
+
+}
